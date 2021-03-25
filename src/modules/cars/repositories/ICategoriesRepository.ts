805c5db65e9d41bdb3386df7a332d485160e1bc4
Samuel Belo <belo.samuel@gmail.com>
2021-03-25 17:25:01 -0300

    modified:   ormconfig.json 	modified:   src/modules/cars/repositories/ICategoriesRepository.ts 	modified:   src/server.ts

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,16 +1,15 @@
 import { Category } from "../entities/Category";
 
+//DTO => Data transfer object
+interface ICreateCategoryDTO {
+    name: string
+    description: string
+}
 
 interface ICategoriesRepository {
     findByName(name: string): Promise<Category>
     list(): Promise<Category[]>
     create({name, description}: ICreateCategoryDTO): Promise<void>
-}
-
-//DTO => Data transfer object
-interface ICreateCategoryDTO {
-    name: string
-    description: string
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO }
