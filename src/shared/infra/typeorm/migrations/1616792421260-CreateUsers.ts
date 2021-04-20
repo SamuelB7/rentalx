c9f95fcbe773c7e5351540a3e328e3cdb722ed1e
Samuel Belo <belo.samuel@gmail.com>
2021-04-19 23:36:39 -0300

    new file:   src/modules/rentals/dtos/ICreateRentalDTO.ts 	new file:   src/modules/rentals/infra/typeorm/entities/Rental.ts 	new file:   src/modules/rentals/repositories/IRentalsRepository.ts 	new file:   src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts 	new file:   src/modules/rentals/useCases/createRental/CreateRentalController.ts 	new file:   src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts 	new file:   src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts 	modified:   src/shared/infra/typeorm/migrations/1616792421260-CreateUsers.ts 	new file:   src/shared/infra/typeorm/migrations/1618871528484-CreateRentals.ts

--- src/shared/infra/typeorm/migrations/1616792421260-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1616792421260-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1616792421260 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
