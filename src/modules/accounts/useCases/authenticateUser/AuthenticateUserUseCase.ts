9ad4ac0d48de7ff12c4b77058e1c038a785f7ddf
Samuel Belo <belo.samuel@gmail.com>
2021-05-03 16:29:11 -0300

    new file:   src/config/auth.ts 	new file:   src/modules/accounts/dtos/ICreateUserTokenDTO.ts 	new file:   src/modules/accounts/infra/typeorm/entities/UserTokens.ts 	new file:   src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts 	new file:   src/modules/accounts/repositories/IUsersTokensRepository.ts 	modified:   src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts 	modified:   src/shared/container/index.ts 	modified:   src/shared/container/providers/DateProvider/IDateProvider.ts 	modified:   src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts 	new file:   src/shared/infra/typeorm/migrations/1620042181608-CreateUsersToken.ts

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,6 +4,9 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 import { AppError } from "../../../../shared/errors/AppError";
+import { IUsersTokensRepository } from "../../repositories/IUsersTokensRepository";
+import auth from "../../../../config/auth";
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 
 interface IRequest {
     email: string
@@ -16,17 +19,29 @@ interface IResponse {
         email: string
     },
     token: string
+    refresh_token: string
 }
 
 @injectable()
 class AuthenticateUserUseCase {
     constructor(
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject("UsersTokensRepository")
+        private usersTokensRepository: IUsersTokensRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
     ){} 
 
     async excute({email, password}: IRequest): Promise<IResponse> {
         const user = await this.usersRepository.findByEmail(email)
+        const { expires_in_token, 
+                secret_refresh_token, 
+                secret_token, 
+                expires_in_refresh_token ,
+                expires_refresh_token_days
+            } = auth
+
         if(!user) {
             throw new AppError("Email or password incorrect!")
         }
@@ -36,9 +51,22 @@ class AuthenticateUserUseCase {
             throw new AppError("Email or password incorrect!")
         }
         
-        const token = sign({}, "69c6e4a394b794ac45a4ca63761bff01", {
+        const token = sign({}, secret_token, {
             subject: user.id,
-            expiresIn: "1d"
+            expiresIn: expires_in_token
+        })
+
+        const refresh_token = sign({ email }, secret_refresh_token, {
+            subject: user.id,
+            expiresIn: expires_in_refresh_token
+        })
+
+        const refresh_token_expires_date = this.dateProvider.addDays(expires_refresh_token_days)
+
+        await this.usersTokensRepository.create({
+            user_id: user.id,
+            refresh_token,
+            expires_date: refresh_token_expires_date,
         })
 
         const tokenReturn: IResponse = {
@@ -46,7 +74,8 @@ class AuthenticateUserUseCase {
             user: {
                 name: user.name,
                 email: user.email
-            }
+            },
+            refresh_token
         }
 
         return tokenReturn
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -14,6 +14,8 @@ import { ICarImagesRepository } from '../../modules/cars/repositories/ICarsImage
 import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImagesRepository'
 import { IRentalsRepository } from '../../modules/rentals/repositories/IRentalsRepository'
 import { RentalsRepository } from '../../modules/rentals/infra/typeorm/repositories/RentalsRepository'
+import { IUsersTokensRepository } from '../../modules/accounts/repositories/IUsersTokensRepository'
+import { UsersTokensRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository'
 
 
 // ICategoriesRepository
@@ -45,4 +47,9 @@ container.registerSingleton<ICarImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   "RentalsRepository",
   RentalsRepository
+)
+
+container.registerSingleton<IUsersTokensRepository>(
+    "UsersTokensRepository",
+    UsersTokensRepository
 )
