a9d7aed382372d76d136cb110aedcf124d52ab5c
Samuel Belo <belo.samuel@gmail.com>
2021-05-05 16:02:13 -0300

    new file:   src/modules/accounts/repositories/im-memory/UsersTokensRepositoryInMemory.ts 	modified:   src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts 	modified:   src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts 	modified:   src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,17 +1,27 @@
+import { DayjsDateProvider } from "../../../../shared/container/providers/DateProvider/implementations/DayjsDateProvider"
 import { AppError } from "../../../../shared/errors/AppError"
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
 import { UsersRepositoryInMemory } from "../../repositories/im-memory/UsersRepositoryInMemory"
+import { UsersTokensRepositoryInMemory } from "../../repositories/im-memory/UsersTokensRepositoryInMemory"
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 
 let authenticateUserUseCase: AuthenticateUserUseCase
 let usersRepositoryInMemory: UsersRepositoryInMemory
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory
+let dateProvider: DayjsDateProvider
 let createUserUseCase: CreateUserUseCase
 
 describe("Authenticate User", () => {
     beforeEach(() => {
         usersRepositoryInMemory = new UsersRepositoryInMemory()
-        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory)
+        usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory()
+        dateProvider = new DayjsDateProvider()
+        authenticateUserUseCase = new AuthenticateUserUseCase(
+            usersRepositoryInMemory, 
+            usersTokensRepositoryInMemory,
+            dateProvider
+        )
         createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
     })
     it("should be able to authenticate an user", async () => {
